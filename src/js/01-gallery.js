d9aa9d1885683b3a80761bd6deb9c0f4d7543f00
SvitLana Borovets <svetaborovets@ukr.net>
2022-11-02 00:19:16 +0200

    add first-finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,31 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(SimpleLightbox);
+const itemTemplate = ({
+  preview,
+  original,
+  description,
+}) => `<li> <a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>
+</li>
+`;
+const refs = {
+  galleryElem: document.querySelector('.gallery'),
+};
+
+const render = () => {
+  const galleryList = galleryItems.map(item => itemTemplate(item)).join('');
+  refs.galleryElem.innerHTML = '';
+  refs.galleryElem.insertAdjacentHTML('beforeend', galleryList);
+};
+render();
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
